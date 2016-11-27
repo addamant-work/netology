05515f60701dbfef72aa24bdd1e7fc0cdbf28c58
addamant-work <addamant@mail.ru>
2016-11-27 20:28:27 +0600

    Update index.php
    Заменил echo на exit

--- index.php
+++ index.php
@@ -4,13 +4,13 @@ echo "Загаданное число: " . $a . "!<br /><br />";
 if (isset($_POST['number'])) {
   $b = (int) $_POST['number'];
   if ($b > $a) {
-    echo "<p>Слишком много</p>";
+    exit ("<p>Слишком много!</p>");
   }
   if ($b < $a) {
-      echo "<p>Слишком мало</p>";
+    exit ("<p>Слишком мало!</p>");
   }
   if ($b === $a) {
-      echo "<p>Вы угадали</p>";
+    echo "<p>Вы угадали</p>";
   }
 }
 else {
@@ -19,5 +19,5 @@ else {
 ?>
 <form action method="post">
   <input name="number">
-  <button>Отправить</button>
+  <button>Совпало?!</button>
 </form>
